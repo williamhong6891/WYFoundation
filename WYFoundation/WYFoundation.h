1d1c3b62d0bd29982b2d51462912366fe3637be7
William Yeung <williamhong6891@gmail.com>
2018-09-25 22:55:06 +0800

    Project and dependencies set up

---
+++ WYFoundation/WYFoundation.h
@@ -0,0 +1,18 @@
+//
+//  WYFoundation.h
+//  WYFoundation
+//
+//  Created by William Yeung on 18/25/9/.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WYFoundation.
+FOUNDATION_EXPORT double WYFoundationVersionNumber;
+
+//! Project version string for WYFoundation.
+FOUNDATION_EXPORT const unsigned char WYFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WYFoundation/PublicHeader.h>
+
+
